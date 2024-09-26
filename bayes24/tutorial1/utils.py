3a591c5886bf2f43f95afc343b3de42799c15b15
t-kist <39705836+t-kist@users.noreply.github.com>
2024-09-26 18:53:11 +0200

    added setup and started first tutorial

---
+++ bayes24/tutorial1/utils.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from IPython import embed
+
+def get_favorite_number(name):
+    """
+    Print the favorite number of a person.
+
+    Args:
+        name (str):
+            The name of the person.
+
+    Returns:
+        n (float):
+            The favorite number of the person.
+    """
+
+    if name == 'Alice':
+        n = np.pi
+    elif name == 'Bob':
+        n = np.random.randint(1, 100)
+    elif name == 'Timo':
+        n = 42
+    else:
+        raise ValueError("Person {:s} not recognized.".format(name))
+
+    return n
