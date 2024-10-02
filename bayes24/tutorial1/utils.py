4d5badbc23b5b3492fa91b018770dfb8970bc624
JelmerPastoor <jelmer.pastoor@gmail.com>
2024-10-02 18:34:43 +0200

    Test thing

--- bayes24/tutorial1/utils.py
+++ bayes24/tutorial1/utils.py
@@ -21,6 +21,8 @@ def get_favorite_number(name):
         n = np.random.randint(1, 100)
     elif name == 'Timo':
         n = 42
+    elif name == 'Jelmer':
+        n = 27
     else:
         raise ValueError("Person {:s} not recognized.".format(name))
 
